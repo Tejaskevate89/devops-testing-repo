08bfb9dd192174340476c151bf25ab126503be2a
TEJAS <tejaskevate89@gmail.com>
2026-01-31 13:13:25 +0530

    ami change

--- variable.tf
+++ variable.tf
@@ -2,7 +2,7 @@ variable "vpc_id" {
   default = "vpc-038a56f95e3f8a2de"
 }
 variable "ami_id" {
-  default = "ami-019715e0d74f695be"
+  default = "ami-0ff5003538b60d5ec"
 }
 variable "instance_type" {
   default = "t2.nano"
